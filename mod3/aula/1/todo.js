bee9c053b6552f8aa0f7fff2b51709470bb3ab3f
unknown <maria_longo@saojose.sc.senai.br>
2019-06-17 08:44:06 -0300

    modulo 3 aula 2

---
+++ mod3/aula/1/todo.js
@@ -0,0 +1,7 @@
+var listElement=document.querySelector("#app ul");
+var InputElement=document.querySelector("#app input");
+var ButtonElement=document.querySelector("#app button");
+
+console.log(listElement);
+console.log(InputElement);
+console.log(ButtonElement);
